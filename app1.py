3610c01e49c875486d87e014b3431bd80a93fc84
KUSHAL GARG <66581889+KushalGarg12@users.noreply.github.com>
2020-11-01 14:44:25 +0530

    SMS Classifier

---
+++ app1.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+
+
+
+
+app = Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+cv=pickle.load(open('transform.pkl','rb'))
+
+
+#cv = CountVectorizer()
+@app.route('/')
+def home():
+	return render_template('home.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+	
+	if request.method == 'POST':
+		message1= request.form['message']
+		data1 = [message1]
+		#vect=tfidf.fit(data1)
+		vect = cv.transform(data1).toarray()
+
+		my_prediction = model.predict(vect)
+		return render_template('result.html',prediction = my_prediction)
+		
+
+if __name__ == '__main__':
+	app.run(debug=True)
